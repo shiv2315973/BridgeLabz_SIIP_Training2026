0b50c30d3ad15eff94e829c34adb15262352f8af
shivam <sg0212300@gmail.com>
2026-07-02 21:19:40 +0530

    Add DBMS folder

---
+++ DBMS/RDBMS/UPDATE CASE INFRMOATION.sql
@@ -0,0 +1,8 @@
+set SQL_SAFE_UPDATES =0;
+UPDATE covid_cases
+SET confirmed_cases = 45200000
+WHERE country = 'India'
+AND report_date = '2024-02-01';
+SELECT *
+FROM covid_cases
+WHERE country = 'India';
